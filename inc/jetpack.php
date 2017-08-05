6a9647a3d78cb732624232016fcc2460cd688d69
Kelly Dwan <ryelle@users.noreply.github.com>
2017-08-05 13:53:42 -0400

    Switch to an “archive” template for all archive pages

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -45,10 +45,6 @@ add_action( 'after_setup_theme', 'bostonwp_jetpack_setup' );
 function bostonwp_infinite_scroll_render() {
 	while ( have_posts() ) {
 		the_post();
-		if ( is_search() ) :
-			get_template_part( 'template-parts/content', 'search' );
-		else :
-			get_template_part( 'template-parts/content', get_post_format() );
-		endif;
+		get_template_part( 'template-parts/content', 'archive' );
 	}
 }
